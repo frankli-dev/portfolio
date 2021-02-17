3640a5ee2f1382dff7650b82eff8787a0a12caad
Ice <iestyndevera@gmail.com>
2021-02-18 02:06:22 +0800

    placed location label google maps

--- components/pages/contact/Map/Map.tsx
+++ components/pages/contact/Map/Map.tsx
@@ -12,17 +12,29 @@ const Map: React.FC = () => {
         bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API }}
         defaultCenter={{
           lat: 14.558523749919372,
-          lng: 121.01124911756745,
+          lng: 121.02194911756745,
         }}
         defaultZoom={13}
       >
+        <h2
+          className={styles.manila}
+          // @ts-ignore
+          lat={14.568523749919372}
+          lng={120.9999911756745}
+        >
+          Metro Manila, Philippines
+        </h2>
         <div
           className={styles.marker}
           // @ts-ignore
           lat={14.558523749919372}
           lng={121.05424911756745}
         >
-          <img className={styles.markerLogo} alt="ice logo" src="/ice-logo.png" />
+          <img
+            className={styles.markerLogo}
+            alt="ice logo"
+            src="/ice-logo.png"
+          />
           <FontAwesomeIcon icon={faMapMarker} />
         </div>
       </GoogleMapReact>
